63f362e832a71a416f0ae649f330d6015635d34d
mazwaay <wahyucheger5!@gmal.com>
2022-11-13 13:35:03 +0700

    commit

--- cypress/e2e/43 - Validate Header.cy.js
+++ cypress/e2e/43 - Validate Header.cy.js
@@ -3,8 +3,9 @@ describe('Headers', () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon').then((response) => {
         cy.get('@pokemon').its('headers').its('content-type')
             .should('include', 'application/json; charset=utf-8')
-            expect(response.headers).to.have.property('content-type', 'application/json; charset=utf-8')
             expect(response.status).to.eq(200)
+            expect(response.headers).to.have.property('content-type', 'application/json; charset=utf-8')
+            expect(response.headers).to.have.property('server', 'cloudflare')
             expect(response.body).to.not.be.null
             expect(response.body).to.have.property('abilities')
             expect(response.body).to.have.property('name', 'ditto')
